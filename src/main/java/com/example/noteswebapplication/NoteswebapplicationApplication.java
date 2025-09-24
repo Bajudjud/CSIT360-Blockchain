d2518111d64731f5f5086b03aeb751e496f09940
John Earl Mandawe <johnearl.mandawe@yahoo.com>
2025-09-24 18:26:36 +0800

    unzip and organize Java Spring Boot notes app

---
+++ src/main/java/com/example/noteswebapplication/NoteswebapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteswebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteswebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteswebapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/noteswebapplication/NoteswebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noteswebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteswebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
